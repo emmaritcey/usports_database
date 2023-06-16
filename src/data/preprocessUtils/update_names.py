cee5416b490c76cd4e7bbc2294f1674dd141b407
Emma Ritcey <ritcey.emma@gmail.com>
2023-06-16 12:10:10 -0400

    fix names

--- src/data/preprocessUtils/update_names.py
+++ src/data/preprocessUtils/update_names.py
@@ -20,10 +20,10 @@ def run():
     df.loc[ df['NAME'] == 'Karla Yepez', 'NAME'] = 'Karla Yepez Villamorin'
     df.loc[ df['NAME'] == 'Lyndsay DeGroot', 'NAME'] = 'Lyndsay DeGroot Sutherland'
     df.loc[ df['NAME'] == 'Kim Tulloch', 'NAME'] = 'Kimberley Tulloch'
-    df.loc[ df['NAME'] == 'Saskia Van ginhoven', 'NAME'] = 'Saskia Van Ginhoven'
+    df.loc[ df['NAME'] == 'Saskia Van Ginhoven', 'NAME'] = 'Saskia van Ginhoven'
     df.loc[ df['NAME'] == 'Tessa Ratzlaff', 'NAME'] = 'Tessa Ratzlaff Allison'
-    df.loc[ df['NAME'] == 'Harlem Sidhu', 'NAME'] = 'Harlem Sidhu Dulay'
-    df.loc[ df['NAME'] == 'Jess Brown', 'NAME'] = 'Jessica Brown'
+    df.loc[ df['NAME'] == 'Harleen Sidhu', 'NAME'] = 'Harleen Sidhu Dulay'
+    df.loc[ df['NAME'] == 'Jessie Brown', 'NAME'] = 'Jessica Brown'
     df.loc[ df['NAME'] == 'Mackenzie Farmer', 'NAME'] = 'MacKenzie Farmer'
     df.loc[ df['NAME'] == 'Lisa Furchner', 'NAME'] = 'Lisa Furchner Carruthers'
     df.loc[ df['NAME'] == 'Sasha Polishchuk', 'NAME'] = 'Alexandra (Sasha) Polishchuk'
@@ -32,7 +32,7 @@ def run():
     df.loc[ df['NAME'] == 'Bojana Kovacevic', 'NAME'] = 'Bojana Kovacevic Glisic'
     df.loc[ df['NAME'] == 'M-M Genois', 'NAME'] = 'Marie-Michelle Genois'
     df.loc[ df['NAME'] == 'Michelle A-Bellemare', 'NAME'] = 'Michelle Auger-Bellemare'
-    df.loc[ df['NAME'] == 'Alex Kiss-Rusk', 'NAME'] = 'Alexandia Kiss-Rusk'
+    df.loc[ df['NAME'] == 'Alex Kiss-Rusk', 'NAME'] = 'Alexandria Kiss-Rusk'
     df.loc[ df['NAME'] == 'Khaleann C.-Goudreau', 'NAME'] = 'Khaleann Caron-Goudreau'
     
     df.to_csv('/Users/emmaritcey/Documents/basketball_research/usports_database/data/processed/players.csv', 
